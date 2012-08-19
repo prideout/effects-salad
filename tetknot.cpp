4e687f80428d0daa096a57b63812f84f53b0ba5f
Philip Rideout <prideout@gmail.com>
2012-08-19 13:48:16 -0700

    generate caps for the wheel shape

--- tetknot.cpp
+++ tetknot.cpp
@@ -25,16 +25,18 @@ PezConfig PezGetConfig()
 void GenerateWheel(glm::vec3 center, float radius, float width,
                    int numQuads, tetgenio* dest)
 {
-    dest->numberofpoints = numQuads * 2;
+    dest->numberofpoints = numQuads * 2 + 2;
     dest->pointlist = new float[dest->numberofpoints * 3];
     const float twopi = 2 * glm::pi<float>();
     const float dtheta = twopi / numQuads;
     float* coord = dest->pointlist;
+    const float z0 = -width / 2;
+    const float z1 = width / 2;
+    
+    // Rim points:
     for (float theta = 0; theta < twopi - dtheta / 2; theta += dtheta) {
         float x = radius * std::cos(theta);
         float y = radius * std::sin(theta);
-        float z0 = -width / 2;
-        float z1 = width / 2;
         *coord++ = x;
         *coord++ = y;
         *coord++ = z0;
@@ -42,19 +44,40 @@ void GenerateWheel(glm::vec3 center, float radius, float width,
         *coord++ = y;
         *coord++ = z1;
     }
+    // Top cap center:
+    *coord++ = 0;
+    *coord++ = 0;
+    *coord++ = z0;
+    // Bottom cap center:
+    *coord++ = 0;
+    *coord++ = 0;
+    *coord++ = z1;
 
-    dest->numberoftrifaces = numQuads * 2;
+    int trisPerCap = numQuads;
+    dest->numberoftrifaces = numQuads * 2 + trisPerCap * 2;
     dest->trifacelist = new int[dest->numberoftrifaces * 3];
     int numPoints = dest->numberofpoints;
-    int numTris = dest->numberoftrifaces;
     int* corner = dest->trifacelist;
-    for (int n = 0; n < numTris; n += 2) {
+    // Rim strip:
+    for (int n = 0; n < numQuads * 2; n += 2) {
         *corner++ = n;
         *corner++ = n+1;
         *corner++ = (n+2) % numPoints;
         *corner++ = (n+2) % numPoints;
         *corner++ = n+1;
         *corner++ = (n+3) % numPoints;
+    }
+    // Top cap fan:
+    for (int n = 0; n < numQuads * 2; n += 2) {
+        *corner++ = n;
+        *corner++ = n + 2;
+        *corner++ = numQuads * 2;
+    }
+    // Bottom cap fan:
+    for (int n = 0; n < numQuads * 2; n += 2) {
+        *corner++ = 1 + n;
+        *corner++ = 1 + n + 2;
+        *corner++ = 1 + numQuads * 2;
     }
 }
 
@@ -75,7 +98,6 @@ void PezInitialize()
     //   (1.414), and apply a maximum volume constraint (a0.1).
 
     tetgenio out;
-    //tetrahedralize("pq1.414a0.1", &in, &out);
     tetrahedralize("q1.414a0.1", &in, &out);
 
     // Call tetgen
