9b21d124387b7b8946e889f1a6077d03eee13e22
Philip Rideout <prideout@gmail.com>
2012-08-19 14:16:09 -0700

    success with tetgen!

--- tetknot.cpp
+++ tetknot.cpp
@@ -21,11 +21,13 @@ PezConfig PezGetConfig()
     return config;
 }
 
-// Creates a circular ribbon, composing it out of quads.
+// Creates a circular ribbon, composing the rim out of quads.
+// Each of the two caps is a single "facet" and each quad is a "facet".
+// (tetgen defines a facet as a coplanar set of polygons)
 void GenerateWheel(glm::vec3 center, float radius, float width,
                    int numQuads, tetgenio* dest)
 {
-    dest->numberofpoints = numQuads * 2 + 2;
+    dest->numberofpoints = numQuads * 2;
     dest->pointlist = new float[dest->numberofpoints * 3];
     const float twopi = 2 * glm::pi<float>();
     const float dtheta = twopi / numQuads;
@@ -44,40 +46,39 @@ void GenerateWheel(glm::vec3 center, float radius, float width,
         *coord++ = y;
         *coord++ = z1;
     }
-    // Top cap center:
-    *coord++ = 0;
-    *coord++ = 0;
-    *coord++ = z0;
-    // Bottom cap center:
-    *coord++ = 0;
-    *coord++ = 0;
-    *coord++ = z1;
 
-    int trisPerCap = numQuads;
-    dest->numberoftrifaces = numQuads * 2 + trisPerCap * 2;
-    dest->trifacelist = new int[dest->numberoftrifaces * 3];
-    int numPoints = dest->numberofpoints;
-    int* corner = dest->trifacelist;
-    // Rim strip:
-    for (int n = 0; n < numQuads * 2; n += 2) {
-        *corner++ = n;
-        *corner++ = n+1;
-        *corner++ = (n+2) % numPoints;
-        *corner++ = (n+2) % numPoints;
-        *corner++ = n+1;
-        *corner++ = (n+3) % numPoints;
+    // Facet per rim face + 2 facets for the "caps"
+    dest->numberoffacets = numQuads + 2;
+    dest->facetlist = new tetgenio::facet[dest->numberoffacets];
+    tetgenio::facet* facet = &dest->facetlist[0];
+
+    // Rim faces:
+    for (int n = 0; n < numQuads * 2; n += 2, ++facet) {
+        facet->numberofpolygons = 1;
+        facet->polygonlist = new tetgenio::polygon[facet->numberofpolygons];
+        facet->numberofholes = 0;
+        facet->holelist = NULL;
+        tetgenio::polygon* poly = &facet->polygonlist[0];
+        poly->numberofvertices = 4;
+        poly->vertexlist = new int[poly->numberofvertices];
+        poly->vertexlist[0] = n;
+        poly->vertexlist[1] = n+1;
+        poly->vertexlist[2] = (n+3) % (numQuads*2);
+        poly->vertexlist[3] = (n+2) % (numQuads*2);
     }
-    // Top cap fan:
-    for (int n = 0; n < numQuads * 2; n += 2) {
-        *corner++ = n;
-        *corner++ = n + 2;
-        *corner++ = numQuads * 2;
-    }
-    // Bottom cap fan:
-    for (int n = 0; n < numQuads * 2; n += 2) {
-        *corner++ = 1 + n;
-        *corner++ = 1 + n + 2;
-        *corner++ = 1 + numQuads * 2;
+
+    // Cap fans:
+    for (int cap = 0; cap < 2; ++cap, ++facet) {
+        facet->numberofpolygons = 1;
+        facet->polygonlist = new tetgenio::polygon[facet->numberofpolygons];
+        facet->numberofholes = 0;
+        facet->holelist = NULL;
+        tetgenio::polygon* poly = &facet->polygonlist[0];
+        poly->numberofvertices = numQuads;
+        poly->vertexlist = new int[poly->numberofvertices];
+        for (int q = 0; q < numQuads; ++q) {
+            poly->vertexlist[q] = q * 2 + cap;
+        }
     }
 }
 
@@ -92,15 +93,16 @@ void PezInitialize()
     tetgenio in;
     GenerateWheel(glm::vec3(0), 1.0f, 0.1f, 32, &in);
 
-    // Write a TubeGenerator class that generates verts in the "tetgen" format
+    // Write a TubeGenerator class that generates verts in the "tetgen" format,
+    // similar to GenerateWheel
     // Tetrahedralize the PLC. Switches are chosen to read a PLC (p),
     //   do quality mesh generation (q) with a specified quality bound
     //   (1.414), and apply a maximum volume constraint (a0.1).
 
     tetgenio out;
-    tetrahedralize("q1.414a0.1", &in, &out);
+    tetrahedralize("pq1.414a0.1", &in, &out);
 
-    // Call tetgen
+    // Figure out how to draw a wireframe
     
     // Figure out how to represent the tets in GL:
     //   Static index buffer
