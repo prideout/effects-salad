f40c1a100e04b9c826e3fe6d3f5df49f1a8fe0a4
Jeremy Cowles <jeremy.cowles@gmail.com>
2012-09-28 00:40:48 -0700

    Fixed SDL_mixer header imports for linux

--- common/audio.cpp
+++ common/audio.cpp
@@ -5,7 +5,14 @@
 #include <iostream>
 
 #include <SDL.h>
-#include <SDL_mixer/SDL_mixer.h>
+
+#ifdef __APPLE__
+    #include <SDL_mixer/SDL_mixer.h>
+#else
+    // this works on RHEL, but not sure if it will work on other distros or on
+    // windows
+    #include <SDL/SDL_mixer.h>
+#endif
 
 /* Mix_Music actually holds the music information.  */
 Mix_Music *music = NULL;
