c0f5e85d1d9b2adb188911bbb85332afc285b955
jcowles <jeremy.cowles@gmail.com>
2012-10-02 13:09:02 -0700

    Added random orientation for leaves, would be nice to orient them correctly, but this will do for now

--- fx/tree.cpp
+++ fx/tree.cpp
@@ -23,6 +23,7 @@ void Tree::Init() {
     FloatList leaves;
     FloatList times;
     float leafSize = .02;
+    int leafCount = 3;
 
     FOR_EACH(branchIt, _treeSys.branches) {
         BranchDef* branch = *branchIt;
@@ -56,69 +57,64 @@ void Tree::Init() {
 
             tube->Init();
         } else {
-            // make a leaf instead
-            times.push_back( (.5 - (rand() / float(RAND_MAX))*.5) 
-                            + (growTime / maxLevel) 
-                            * (maxLevel 
-                                - branch->levels 
-                                - ((branch->levels > 0) ? .8 : 0)
-                                + branch->parentPercent)
-                            );
-            times.push_back((growTime / maxLevel) * (branch->levels));
+            for (int leaf_i = 0; leaf_i < leafCount; leaf_i++) {
+                // make a leaf instead
+                times.push_back( (.5 - (rand() / float(RAND_MAX))*.5) 
+                                + (growTime / maxLevel) 
+                                * (maxLevel 
+                                    - branch->levels 
+                                    - ((branch->levels > 0) ? .8 : 0)
+                                    + branch->parentPercent)
+                                );
+                times.push_back((growTime / maxLevel) * (branch->levels));
 
-            glm::vec3 start = branch->cvs[0];
-            branch->norm /= 5.0f;
-            leaves.push_back(start.x - leafSize);
-            leaves.push_back(start.y - leafSize);
-            leaves.push_back(start.z - leafSize);
+                // 
+                // Setup the leaf quad and give it some random orientation
+                // would be nice to orient the leaf to the branch here, which
+                // we could do by using the basis vector the tube would have used
+                // 
+                glm::vec3 leafQuad[4];
+                glm::mat3 basis;
 
-            leaves.push_back(start.x + leafSize);
-            leaves.push_back(start.y - leafSize);
-            leaves.push_back(start.z - leafSize);
+                glm::vec3 tangent(.5 - (rand() / float(RAND_MAX))*.5,
+                            .5 - (rand() / float(RAND_MAX))*.5,
+                            .5 - (rand() / float(RAND_MAX))*.5);
+                tangent = glm::normalize(tangent);
+                basis[2] = tangent;
 
-            leaves.push_back(start.x - leafSize);
-            leaves.push_back(start.y + leafSize);
-            leaves.push_back(start.z - leafSize);
+                glm::vec3 norm;
+                glm::vec3 v = glm::vec3(1,0,0);
+                norm = glm::cross(tangent,v);
+                float e = glm::dot(norm,norm);
+                if (e < 0.01) {
+                    v = glm::vec3(0,1,0); 
+                    norm = glm::cross(tangent,v);
+                }
+                basis[0] = glm::normalize(norm);
+                basis[1] = glm::normalize(glm::cross(tangent, norm));
 
+                // --, +-, ++, -+
+                for (int i = 0; i < 4; i++) {
+                    leafQuad[i].x = (2*(i & 1) - 1.0) * leafSize;
+                    leafQuad[i].y = (2*(i & 2) - 1.0) * leafSize;
+                    leafQuad[i].z = 0.0;
+                    leafQuad[i] = leafQuad[i] * basis;
+                }
 
-            leaves.push_back(start.x + leafSize);
-            leaves.push_back(start.y - leafSize);
-            leaves.push_back(start.z - leafSize);
+                glm::vec3 start = branch->cvs[0];
+                branch->norm /= 5.0f;
+                for (int q = 0; q < 3; q++) {
+                    leaves.push_back(start.x + leafQuad[q].x);
+                    leaves.push_back(start.y + leafQuad[q].y);
+                    leaves.push_back(start.z + leafQuad[q].z);
+                }
 
-            leaves.push_back(start.x - leafSize);
-            leaves.push_back(start.y + leafSize);
-            leaves.push_back(start.z - leafSize);
-            
-            leaves.push_back(start.x + leafSize);
-            leaves.push_back(start.y + leafSize);
-            leaves.push_back(start.z - leafSize);
-
-
-
-            leaves.push_back(start.x - leafSize);
-            leaves.push_back(start.y - leafSize);
-            leaves.push_back(start.z - leafSize);
-
-            leaves.push_back(start.x - leafSize);
-            leaves.push_back(start.y - leafSize);
-            leaves.push_back(start.z + leafSize);
-
-            leaves.push_back(start.x - leafSize);
-            leaves.push_back(start.y + leafSize);
-            leaves.push_back(start.z - leafSize);
-
-
-            leaves.push_back(start.x - leafSize);
-            leaves.push_back(start.y - leafSize);
-            leaves.push_back(start.z + leafSize);
-
-            leaves.push_back(start.x - leafSize);
-            leaves.push_back(start.y + leafSize);
-            leaves.push_back(start.z - leafSize);
-            
-            leaves.push_back(start.x - leafSize);
-            leaves.push_back(start.y + leafSize);
-            leaves.push_back(start.z + leafSize);
+                for (int q = 3; q > 0; q--) {
+                    leaves.push_back(start.x + leafQuad[q].x);
+                    leaves.push_back(start.y + leafQuad[q].y);
+                    leaves.push_back(start.z + leafQuad[q].z);
+                }
+            }
         }
     }
     _leaves = Vao(3, leaves);
