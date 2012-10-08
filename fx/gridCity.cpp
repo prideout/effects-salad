bc37db48e301bb8182095a870cf27bb2ce4776a9
Philip Rideout <prideout@gmail.com>
2012-10-07 20:09:06 -0700

    grid of buildings

--- fx/gridCity.cpp
+++ fx/gridCity.cpp
@@ -19,6 +19,9 @@ static const int TerrainSize = 150;
 static const float TerrainScale = 0.5;
 static const float MinHeight = 7;
 static const float MaxHeight = 10;
+static const int NumRows = 10;
+static const int NumCols = 10;
+static const vec2 CellScale = vec2(0.9f, 0.7f);
 
 GridCity::GridCity()
 {
@@ -29,6 +32,41 @@ GridCity::~GridCity()
 }
 
 extern vec3 MyTerrainFunc(vec2 v);
+
+HalfBeat::HalfBeat()
+{
+    _previousBeatTime = 0;
+    _currentBeatInterval = 0;
+    _downBeat = false;
+    _multiplier = 2.0f;
+}
+
+bool HalfBeat::Update(bool beat, float time)
+{
+    if (beat) {
+        _currentBeatInterval = time - _previousBeatTime;
+        _previousBeatTime = time;
+        _downBeat = false;
+        return true;
+    }
+    if (_downBeat) {
+        return false;
+    }
+    float dt = time - _previousBeatTime;
+    if (dt * _multiplier > _currentBeatInterval) {
+        _downBeat = true;
+        return true;
+    }
+    return false;
+}
+
+vec2 GridCity::_CellSample(int row, int col)
+{
+    float s = TerrainSize;
+    float x = float(col) / NumCols;
+    float y = float(row) / NumRows;
+    return vec2(-s/2 + x*s, -s/2 + y*s);
+}
 
 void GridCity::Init()
 {
@@ -42,6 +80,44 @@ void GridCity::Init()
     _terrainVao = Vao(3, ground, indices);
     _terrainVao.AddVertexAttribute(AttrNormal, 3, normals);
 
+    // Form the grid
+    _cells.reserve(NumRows * NumCols);
+    GridCell cell;
+    for (int row = 0; row < NumRows; ++row) {
+        for (int col = 0; col < NumCols; ++col) {
+            vec2 nw = _CellSample(row, col);
+            vec2 sw = _CellSample(row+1, col);
+            vec2 ne = _CellSample(row, col+1);
+            vec2 se = _CellSample(row+1, col+1);
+            vec2 n = (nw + ne) / 2.0f;
+            vec2 s = (sw + se) / 2.0f;
+            vec2 w = (nw + sw) / 2.0f;
+            vec2 e = (se + ne) / 2.0f;
+            vec2 p1 = (n + s) / 2.0f;
+            vec2 p2 = (e + w) / 2.0f;
+            vec2 p = (p1 + p2) / 2.0f;
+            vec2 u = CellScale.x * (e - p);
+            vec2 v = CellScale.y * (s - p);
+            cell.Quad.p = vec3(p.x, 0, p.y);
+            cell.Quad.u = vec3(u.x, 0, u.y);
+            cell.Quad.v = vec3(v.x, 0, v.y);
+            cell.Height = MinHeight;
+            _cells.push_back(cell);
+        }
+    }
+
+    // "Unfloat" the quads
+
+    // Seed the sketch objects
+    FOR_EACH(i, _cells) {
+        GridCell& cell = *i;
+        _AllocCell(&cell);
+        cell.Visible = true;
+        cell.CpuTriangles->PullFromScene();
+        cell.CpuTriangles->PushToGpu(cell.GpuTriangles);
+        _FreeCell(&cell);
+    }
+
     // Compile shaders
     Programs& progs = Programs::GetInstance();
     progs.Load("Buildings.Terrain", false);
@@ -54,8 +130,41 @@ void GridCity::Init()
     _camera.eye = vec3(0, 200, 20);
 }
 
+void GridCity::_AllocCell(GridCell* cell)
+{
+    sketch::Scene* shape = new sketch::Scene;
+    const sketch::Plane* ground = shape->GroundPlane();
+ 
+    // TODO prideout add new entry to sketch::Scene to support orientable quads
+ 
+    vec2 offset = vec2(cell->Quad.p.x, cell->Quad.p.z);
+    cell->Roof = shape->AddRectangle(
+        length(cell->Quad.u)*2,
+        length(cell->Quad.v)*2,
+        ground->Eqn,
+        offset);
+
+    shape->PushPath(cell->Roof, cell->Height);
+
+    cell->CpuShape = shape;
+    cell->CpuTriangles = new sketch::Tessellator(*shape);
+}
+
+void GridCity::_FreeCell(GridCell* cell)
+{
+    delete cell->CpuShape;
+    delete cell->CpuTriangles;
+    cell->CpuShape = 0;
+    cell->CpuTriangles = 0;
+}
+
 void GridCity::Update()
 {
+    bool beat = GetContext()->audio->GetKicks() ||
+        GetContext()->audio->GetSnares();
+    bool halfBeat = _beats.Update(beat, 2.0f);
+    if (halfBeat) {
+    }
 }
 
 void GridCity::Draw()
@@ -76,17 +185,13 @@ void GridCity::Draw()
     glUseProgram(progs["Sketch.Facets"]);
     glUniform3f(u("Scale"), 1, 1, 1);
     glUniform3f(u("Translate"), 0, 0, 0);
-
-    #if 0
-    FOR_EACH(e, _elements) {
-        if (not e->Visible) {
+    glUniform1i(u("Smooth"), 0);
+    _camera.Bind(glm::mat4());
+    FOR_EACH(cell, _cells) {
+        if (not cell->Visible) {
             continue;
         }
-        e->GpuTriangles.Bind();
-        mat4 xlate = glm::translate(e->Position);
-        _camera.Bind(xlate);
-        glUniform1i(u("Smooth"), 0);
-        glDrawElements(GL_TRIANGLES, e->GpuTriangles.indexCount, GL_UNSIGNED_INT, 0);
+        cell->GpuTriangles.Bind();
+        glDrawElements(GL_TRIANGLES, cell->GpuTriangles.indexCount, GL_UNSIGNED_INT, 0);
     }
-    #endif
 }
--- fx/gridCity.h
+++ fx/gridCity.h
@@ -7,6 +7,37 @@
 #include "common/camera.h"
 #include "glm/glm.hpp"
 
+// Orientable rectangle in 3-space define by a point and two vectors:
+// p ... center point
+// u ... half-width vector
+// v ... half-height vector
+struct GridQuad {
+    glm::vec3 p;
+    glm::vec3 u;
+    glm::vec3 v;
+};
+
+struct GridCell {
+    GridQuad Quad;
+    float Height;
+    sketch::Scene* CpuShape;
+    sketch::Tessellator* CpuTriangles;
+    sketch::CoplanarPath* Roof;
+    Vao GpuTriangles;
+    bool Visible;
+ };
+
+typedef std::vector<GridCell> GridCells;
+
+struct HalfBeat {
+    HalfBeat();
+    bool Update(bool beat, float time);
+    float _previousBeatTime;
+    float _currentBeatInterval;
+    bool _downBeat;
+    float _multiplier;
+};
+
 class GridCity : public Effect {
 public:
     GridCity();
@@ -15,8 +46,11 @@ public:
     virtual void Update();
     virtual void Draw();
 private:
-    sketch::Tessellator* _tess;
-    sketch::Playback* _player;
+    vec2 _CellSample(int row, int col);
+    void _AllocCell(GridCell* cell);
+    void _FreeCell(GridCell* cell);
+    HalfBeat _beats;
+    GridCells _cells;
     Vao _terrainVao;
     PerspCamera _camera;
 };
