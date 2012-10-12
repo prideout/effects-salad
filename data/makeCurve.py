2746bba5966f32fce77beb34f619feeea9a095f0
jcowles <jeremy.cowles@gmail.com>
2012-10-12 02:00:00 -0700

    update make curve script

--- data/makeCurve.py
+++ data/makeCurve.py
@@ -5,25 +5,32 @@ import sys
 def Main():
     line = sys.stdin.readline()
     switching = True
+    count = 0
+    lines = []
     while line:
         lineToks = line.strip().split(" ")
         if len(lineToks) == 3:
             try:
                 float(lineToks[0])
                 if switching:
-                    print "cvs.clear();"
+                    lines.append("cvs.clear();")
                 switching = False
-                print "cvs.push_back(vec3(", ",".join(lineToks), "));"
+                lines.append("cvs.push_back(vec3(" + ",".join(lineToks) + "));")
+                count += 1
             except ValueError:
                 switching = True
         else:
-            if not switching:
+            if not switching and count > 3:
+                for l in lines:
+                    print l
                 print "_tubes.push_back(Tube());"
                 print "_tubes.back().cvs = cvs;"
                 print "_tubes.back().radius = .1;"
                 print "_tubes.back().uniformWidth = true;"
                 print "_tubes.back().Init();"
             switching = True
+            lines = []
+            count = 0
         line = sys.stdin.readline()
         
 
